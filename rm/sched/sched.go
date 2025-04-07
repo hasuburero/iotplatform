ade77c2cd8c8b5c086085205ea45ae1e4f4f60cc
hasuburero <hasuburero0802@gmail.com>
2025-04-07 19:51:10 +0900

    adding workerenqueue depends on scheduling worker

--- rm/sched/sched.go
+++ rm/sched/sched.go
@@ -1,69 +1,101 @@
 package sched
 
 import (
-	"github.com/hasuburero/util/panic"
-	"time"
+	"errors"
 	"sched/runtime_match"
 	"sync"
+	"time"
+
+	"github.com/hasuburero/util/panic"
 )
 
 type AccessController_interface interface{}
 
-type Worker_Struct struct{
+type Worker_Struct struct {
 	Worker_id string
-	Runtime []string
+	Runtime   []string
 }
 
-type Job_Struct struct{
-	Job_id string
-	Data1_id string
-	Data2_id string
+type Job_Struct struct {
+	Job_id      string
+	Data1_id    string
+	Data2_id    string
 	Function_id string
-	Runtime string
+	Runtime     string
+	Status      string
 }
 
-type Scheduling_Struct struct {
+type Scheduled_Struct struct {
 	Worker Worker_Struct
 	Job    Job_Struct
 }
 
-type Scheduled_Worker_Struct struct {
+type Scheduling_Worker_Struct struct {
 	Worker Worker_Struct
-	Chan   chan Scheduling_Struct
-	Error  error
+	Chan   chan Scheduled_Struct
+	Error  chan error
 }
 
-type Enqueue_Job_Struct struct {
+type Scheduling_Job_Struct struct {
 	Job   Job_Struct
 	Error error
 }
 
-type Retry_Struct struct{
-	Worker Worker_Struct
-	Job Job_Struct
+type Enqueue_Worker_Struct struct {
+	Scheduling_Worker_Struct
 }
+
+type Enqueue_Job_Struct struct {
+	Scheduling_Job_Struct
+}
+
+type Retry_Struct struct {
+	Worker Worker_Struct
+	Job    Job_Struct
+}
+
+// const definition
+const ()
 
 // var definition
 var AccessMux sync.Mutex
+var WorkerQueue []*Scheduling_Worker_Struct
+var JobQueue []*Job_Struct
 
 // func definition
-func WorkerScheduling(arg Worker_Struct) (Scheduling_Struct, error) {
-	var worker_sched = Scheduled_Worker_Struct{Worker: arg, Chan: make(chan Scheduling_Struct)}
-	select{
-	case <-worker_sched.Chan:
-	case time.
-	}
-	return 
+func WorkerEnqueue(arg Enqueue_Worker_Struct) {
+	go func() {
+		v := AccessController(arg)
+		if v == nil {
+			arg.Error <- errors.New("Returned nil interface")
+			return
+		}
+	}()
 }
 
-func JobEnqueue(arg Scheduling_Struct) error {}
+func JobEnqueue(arg Enqueue_Job_Struct) error {
+}
+func Retry() {}
 
 func AccessController(arg AccessController_interface) AccessController_interface {
 	AccessMux.Lock()
 	var return_value AccessController_interface
 	switch v := arg.(type) {
-	case Scheduled_Worker_Struct:
+	case Enqueue_Worker_Struct:
+		enqueue := v
+		var worker = new(Scheduling_Worker_Struct)
+		worker.Worker = enqueue.Worker
+		worker.Chan = enqueue.Chan
+		worker.Error = enqueue.Error
+		WorkerQueue = append(WorkerQueue, worker)
+		return_value = enqueue
 	case Enqueue_Job_Struct:
+		enqueue := v
+		var job = new(Job_Struct)
+		*job = enqueue.Job
+		JobQueue = append(JobQueue, job)
+		return_value = enqueue
+	case Retry_Struct:
 	}
 
 	AccessMux.Unlock()
